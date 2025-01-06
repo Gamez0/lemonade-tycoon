f901ee0599137dc3d0e29e93c5bd4f44b883ffdd
dobin <dobinshin@gmail.com>
2025-01-06 18:38:06 +0900

    u: Phaser.Events.EventEmitter 기반으로 수정

--- src/models/supplies.ts
+++ src/models/supplies.ts
@@ -1,11 +1,12 @@
-// src/models/Supplies.ts
-export class Supplies {
+import Phaser from "phaser";
+export class Supplies extends Phaser.Events.EventEmitter {
     private _lemon: number;
     private _sugar: number;
     private _ice: number;
     private _cup: number;
 
     constructor(lemon: number, sugar: number, ice: number, cup: number) {
+        super();
         this._lemon = lemon;
         this._sugar = sugar;
         this._ice = ice;
@@ -16,31 +17,35 @@ export class Supplies {
         return this._lemon;
     }
 
-    set lemon(value: number) {
+    setLemon(value: number) {
         this._lemon = value;
+        this.emit('lemonAmountChanged', this._lemon);
     }
 
     get sugar(): number {
         return this._sugar;
     }
 
-    set sugar(value: number) {
+    setSugar(value: number) {
         this._sugar = value;
+        this.emit('sugarAmountChanged', this._sugar);
     }
 
     get ice(): number {
         return this._ice;
     }
 
-    set ice(value: number) {
+    setIce(value: number) {
         this._ice = value;
+        this.emit('iceAmountChanged', this._ice);
     }
 
     get cup(): number {
         return this._cup;
     }
 
-    set cup(value: number) {
+    setCup(value: number) {
         this._cup = value;
+        this.emit('cupAmountChanged', this._cup);
     }
 }
