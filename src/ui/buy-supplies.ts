dba8b735d7193254ab076fbec424f3ad3916a949
dobin <dobinshin@gmail.com>
2025-01-06 17:48:03 +0900

    u: Container -> Buy*

--- src/ui/buy-supplies.ts
+++ src/ui/buy-supplies.ts
@@ -1,4 +1,7 @@
-import { BuyLemon } from "./buy-lemon";
+import { BuyCups } from "./buy-cups";
+import { BuyIce } from "./buy-ice";
+import { BuyLemons } from "./buy-lemons";
+import { BuySugar } from "./buy-sugar";
 import { TabUI } from "./tab-ui";
 
 export class BuySupplies extends Phaser.GameObjects.Container {
@@ -6,13 +9,13 @@ export class BuySupplies extends Phaser.GameObjects.Container {
     private description: Phaser.GameObjects.Text;
     tabUI: TabUI;
     selectedTabIndex: number;
-    buyLemonUI: BuyLemon;
-    tempUI: BuyLemon;
-    private buySugarUI: Phaser.GameObjects.Container;
-    private buyIceUI: Phaser.GameObjects.Container;
-    private buyCupsUI: Phaser.GameObjects.Container;
+    private buyLemonUI: BuyLemons;
+    private buySugarUI: BuySugar;
+    private buyIceUI: BuyIce;
+    private buyCupsUI: BuyCups;
+    private purchaseSupplies: Function;
 
-    constructor(scene: Phaser.Scene, x: number, y: number) {
+    constructor(scene: Phaser.Scene, x: number, y: number, purchaseSupplies: Function) {
         super(scene, x, y);
         this.title = scene.add.text(0, 0, 'Buy Supplies', { fontSize: '24px' });
         this.description = scene.add.text(0, 25, 'Select the supplies you want to buy', { fontSize: '16px' });
@@ -21,11 +24,14 @@ export class BuySupplies extends Phaser.GameObjects.Container {
         this.tabUI.on('tabSelected', this.onTabSelected, this);
         this.selectedTabIndex = 0; // Initialize selectedTabIndex
 
-        this.buyLemonUI = new BuyLemon(scene, 0, 100, 12, 4.8);
-        this.tempUI = new BuyLemon(scene, 0, 100, 0, 2);
-        
+        this.buyLemonUI = new BuyLemons(scene, 0, 100);
+        this.buySugarUI = new BuySugar(scene, 0, 100);
+        this.buyIceUI = new BuyIce(scene, 0, 100);
+        this.buyCupsUI = new BuyCups(scene, 0, 100);
 
-        this.add([this.title, this.description, this.tabUI, this.buyLemonUI, this.tempUI]);
+        this.purchaseSupplies = purchaseSupplies;
+
+        this.add([this.title, this.description, this.tabUI, this.buyLemonUI, this.buySugarUI, this.buyIceUI, this.buyCupsUI]);
         scene.add.existing(this);
         this.updateUI();
     }
@@ -35,29 +41,41 @@ export class BuySupplies extends Phaser.GameObjects.Container {
         this.updateUI();
     }
 
+    // TODO: buy button 만들어야 함
+
+    onBuyButtonClicked() {
+        // check selected items
+        const lemonTotal = this.buyLemonUI.getTotal();
+        const sugarTotal = this.buySugarUI.getTotal();
+        const iceTotal = this.buyIceUI.getTotal();
+        const cupsTotal = this.buyCupsUI.getTotal();
+
+        this.purchaseSupplies(lemonTotal, sugarTotal, iceTotal, cupsTotal);
+
+        // 구매했을 때
+
+        // 구매가 안됐을 때
+    }
 
     updateUI() {
         // Hide all UI elements
-
         this.buyLemonUI.setVisible(false);
-        this.tempUI.setVisible(false);
-        // this.buySugarUI.setVisible(false);
-        // this.buyIceUI.setVisible(false);
-        // this.buyCupsUI.setVisible(false);
+        this.buySugarUI.setVisible(false);
+        this.buyIceUI.setVisible(false);
+        this.buyCupsUI.setVisible(false);
 
         switch(this.selectedTabIndex) {
             case 0:
                 this.buyLemonUI.setVisible(true);
                 break;
             case 1:
-                this.tempUI.setVisible(true);
-                // this.buySugarUI.setVisible(true);
+                this.buySugarUI.setVisible(true);
                 break;
             case 2:
-                // this.buyIceUI.setVisible(true);
+                this.buyIceUI.setVisible(true);
                 break;
             case 3:
-                // this.buyCupsUI.setVisible(true);
+                this.buyCupsUI.setVisible(true);
                 break;
             default:
                 break;
