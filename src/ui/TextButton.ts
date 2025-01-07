4d4c048b037bdbb6906d2397048a73ee00f7b240
dobin <dobinshin@gmail.com>
2025-01-07 21:37:07 +0900

    c: default style

--- src/ui/TextButton.ts
+++ src/ui/TextButton.ts
@@ -1,7 +1,14 @@
 // TextButton.ts usable in Game.ts
 export class TextButton extends Phaser.GameObjects.Text {
-    constructor(scene: Phaser.Scene, x:number, y:number, text:string, style:Phaser.Types.GameObjects.Text.TextStyle) {
-        super(scene, x, y, text, style);
+    constructor(scene: Phaser.Scene, x:number, y:number, text:string, style?:Phaser.Types.GameObjects.Text.TextStyle) {
+        const defaultStyle: Phaser.Types.GameObjects.Text.TextStyle = {    
+                fontFamily: 'Arial',
+                fontSize: '16px',
+                color: '#ffffff',
+                backgroundColor: '#a5d739',
+                padding: { x: 10, y: 5 }
+        };
+        super(scene, x, y, text, style ?? defaultStyle);
         scene.add.existing(this);
         this.setInteractive();
     }
