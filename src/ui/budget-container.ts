cedbc970cf871f0a5b105cf67f238f2d8d01694b
dobin <dobinshin@gmail.com>
2025-01-20 21:59:05 +0900

    u: use TitleText

--- src/ui/budget-container.ts
+++ src/ui/budget-container.ts
@@ -1,36 +1,22 @@
 import Phaser from "phaser";
 import { Budget } from "../models/budget";
+import { TitleText } from "./title-text";
 
 export class BudgetContainer extends Phaser.GameObjects.Container {
-    private budget: Budget;
     private budgetText: Phaser.GameObjects.Text;
 
     constructor(scene: Phaser.Scene, x: number, y: number, budget: Budget) {
         super(scene, x, y);
 
-        this.budget = budget;
-        this.budgetText = this.createText(scene, "Budget: ", budget.getAmount(), 0);
+        this.budgetText = new TitleText(scene, 0, 12, budget.getAmount().toString());
+
+        budget.on("change", () => {
+            this.budgetText.destroy();
+            this.budgetText = new TitleText(this.scene, 0, 12, budget.getAmount().toFixed(2).toString());
+            this.add(this.budgetText);
+        });
+
         this.add(this.budgetText);
-
-        this.budget.on("change", this.updateBudget, this);
-
         scene.add.existing(this);
-    }
-
-    private createText(scene: Phaser.Scene, label: string, value: number, yOffset: number): Phaser.GameObjects.Text {
-        return scene.add
-            .text(0, yOffset, `${label}${value}`, {
-                fontFamily: "Arial Black",
-                fontSize: 20,
-                color: "#ffffff",
-                stroke: "#000000",
-                strokeThickness: 4,
-                align: "right",
-            })
-            .setOrigin(0.5);
-    }
-
-    public updateBudget(budget: number) {
-        this.budgetText.setText(`Budget: ${budget.toFixed(2)}`);
     }
 }
