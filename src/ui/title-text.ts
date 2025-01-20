4e205da630e09391fd6b49f232c25ab5533f1c51
dobin <dobinshin@gmail.com>
2025-01-20 21:21:59 +0900

    c: TitleText

---
+++ src/ui/title-text.ts
@@ -0,0 +1,17 @@
+export class TitleText extends Phaser.GameObjects.Text {
+    constructor(
+        scene: Phaser.Scene,
+        x: number,
+        y: number,
+        text: string,
+        style?: Phaser.Types.GameObjects.Text.TextStyle,
+    ) {
+        const defaultStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+            fontFamily: "Arial",
+            fontSize: "24px",
+            color: "#ffffff",
+        };
+        super(scene, x, y, text, style ?? defaultStyle);
+        scene.add.existing(this);
+    }
+}
