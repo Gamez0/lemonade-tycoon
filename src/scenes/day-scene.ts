15c03e03ea54f6faeca87de14adecc4cb8d3fa40
dobin <dobinshin@gmail.com>
2025-02-12 16:14:03 +0900

    c: animation test

--- src/scenes/day-scene.ts
+++ src/scenes/day-scene.ts
@@ -4,12 +4,17 @@ import { TextButton } from "../ui/text-button";
 export class DayScene extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     skipButton: TextButton;
+    kid: Phaser.GameObjects.Sprite;
 
     constructor() {
         super("day");
     }
 
-    preload() {}
+    preload() {
+        for (let i = 1; i <= 12; i++) {
+            this.load.image(`kid-girl-${i}`, `assets/characters/kid-girl/kid-girl-${i}.png`);
+        }
+    }
 
     create() {
         this.camera = this.cameras.main;
@@ -17,8 +22,75 @@ export class DayScene extends Scene {
         this.skipButton = new TextButton(this, 410, 700, "SKIP");
         this.skipButton.on("pointerdown", () => {
             this.scene.switch("preparation");
+        });
 
-            // TODO: selling scene 도 있네;;; 둘중 하나만 남기자.
+        this.anims.create({
+            key: "stand-front",
+            frames: [{ key: "kid-girl-1" }],
+            frameRate: 10,
+        });
+        this.anims.create({
+            key: "stand-back",
+            frames: [{ key: "kid-girl-2" }],
+            frameRate: 10,
+        });
+        this.anims.create({
+            key: "stand-right",
+            frames: [{ key: "kid-girl-3" }],
+            frameRate: 10,
+        });
+        this.anims.create({
+            key: "stand-left",
+            frames: [{ key: "kid-girl-4" }],
+            frameRate: 10,
+        });
+
+        this.anims.create({
+            key: "walk-down",
+            frames: [{ key: "kid-girl-5" }, { key: "kid-girl-6" }],
+            frameRate: 6,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "walk-up",
+            frames: [{ key: "kid-girl-7" }, { key: "kid-girl-8" }],
+            frameRate: 6,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "walk-right",
+            frames: [{ key: "kid-girl-9" }, { key: "kid-girl-10" }],
+            frameRate: 6,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "walk-left",
+            frames: [{ key: "kid-girl-11" }, { key: "kid-girl-12" }],
+            frameRate: 6,
+            repeat: -1,
+        });
+
+        this.kid = this.add.sprite(100, 300, "kid-girl-1").setScale(2);
+        this.kid.play("stand-front");
+
+        this.tweens.add({
+            targets: this.kid,
+            x: 500,
+            duration: 3000,
+            ease: "Linear",
+            repeat: -1,
+            yoyo: true,
+
+            onUpdate: (tweens) => {
+                if (tweens.elapsed % 6000 < 3000) {
+                    this.kid.play("walk-right", true);
+                } else if (tweens.elapsed % 6000 >= 3000) {
+                    this.kid.play("walk-left", true);
+                }
+            },
         });
     }
 }
