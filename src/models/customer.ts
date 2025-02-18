e49f98580e5cd549d02416d0c6336db98991d9dc
dobin <dobinshin@gmail.com>
2025-02-18 14:49:59 +0900

    u: spell correction

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,28 +1,28 @@
 export class Customer extends Phaser.Events.EventEmitter {
     private _character: Phaser.GameObjects.Sprite;
-    private _patient: number;
+    private _patience: number;
 
     // 아예 비싸면 줄에 서지 않음
     // 줄에 서있으면 patience가 줄어듬
     // patience가 0이 되면 줄에서 나감
 
-    constructor(character: Phaser.GameObjects.Sprite, patient: number) {
+    constructor(character: Phaser.GameObjects.Sprite, patience: number) {
         super();
         this._character = character;
-        this._patient = patient;
+        this._patience = patience;
     }
 
     getCharacter = (): Phaser.GameObjects.Sprite => {
         return this._character;
     };
 
-    getPatient = (): number => {
-        return this._patient;
+    getPatience = (): number => {
+        return this._patience;
     };
 
-    setPatient = (value: number) => {
-        this._patient = value;
-        this.emit("change", this._patient);
+    setPatience = (value: number) => {
+        this._patience = value;
+        this.emit("change", this._patience);
     };
 
     setCharacter = (value: Phaser.GameObjects.Sprite) => {
