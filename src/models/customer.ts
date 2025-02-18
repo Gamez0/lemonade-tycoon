8770419ecf2a386de8d95fbf79962053186a431d
신도빈의 맥 <dobinshin@gmail.com>
2025-02-18 16:59:44 +0900

    c: add todo, how we gonna set patience

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,6 +1,6 @@
 export class Customer extends Phaser.Events.EventEmitter {
     private _character: Phaser.GameObjects.Sprite;
-    private _patience: number;
+    private _patience: number; // TODO: 얼마나 기다려주는지 확인하고 범위를 정해야 할듯.
 
     // 아예 비싸면 줄에 서지 않음
     // 줄에 서있으면 patience가 줄어듬
