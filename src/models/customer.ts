dd4f21650f8be0a54ffa34da79d9e3b58ca4ffb5
dobin <dobinshin@gmail.com>
2025-02-24 23:57:57 +0900

    d: sprite - add character index

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,32 +1,32 @@
 export class Customer extends Phaser.Events.EventEmitter {
-    private _character: Phaser.GameObjects.Sprite;
+    private _characterIndex: number;
     private _patience: number; // TODO: 얼마나 기다려주는지 확인하고 범위를 정해야 할듯.
 
     // 아예 비싸면 줄에 서지 않음
     // 줄에 서있으면 patience가 줄어듬
     // patience가 0이 되면 줄에서 나감
 
-    constructor(character: Phaser.GameObjects.Sprite, patience: number) {
+    constructor(characterIndex: number, patience: number) {
         super();
-        this._character = character;
+        this._characterIndex = characterIndex;
         this._patience = patience;
     }
 
-    getCharacter = (): Phaser.GameObjects.Sprite => {
-        return this._character;
+    getCharacterIndex = (): number => {
+        return this._characterIndex;
     };
 
     getPatience = (): number => {
         return this._patience;
     };
 
+    setCharacterIndex = (value: number) => {
+        this._characterIndex = value;
+        this.emit("change", this._characterIndex);
+    };
+
     setPatience = (value: number) => {
         this._patience = value;
         this.emit("change", this._patience);
-    };
-
-    setCharacter = (value: Phaser.GameObjects.Sprite) => {
-        this._character = value;
-        this.emit("change", this._character);
     };
 }
