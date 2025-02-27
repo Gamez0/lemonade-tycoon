f8f78c2b23dc505700808c6bcfd4fa6a57882b23
dobin <dobinshin@gmail.com>
2025-02-28 03:15:17 +0900

    r: move defaultStyle to the bottom

--- src/ui/title-text.ts
+++ src/ui/title-text.ts
@@ -6,12 +6,13 @@ export class TitleText extends Phaser.GameObjects.Text {
         text: string,
         style?: Phaser.Types.GameObjects.Text.TextStyle
     ) {
-        const defaultStyle: Phaser.Types.GameObjects.Text.TextStyle = {
-            fontSize: "24px",
-            color: "#ffffff",
-            fontFamily: '"Play"',
-        };
         super(scene, x, y, text, { ...defaultStyle, ...style });
         scene.add.existing(this);
     }
 }
+
+const defaultStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+    fontSize: "24px",
+    color: "#ffffff",
+    fontFamily: '"Play"',
+};
