42be97edec3e1a925c116fd07ecc0a786e718638
dobin <dobinshin@gmail.com>
2025-02-28 02:17:57 +0900

    c: add LemonadePitcher class

---
+++ src/models/lemonadePitcher.ts
@@ -0,0 +1,17 @@
+export default class LemonadePitcher extends Phaser.Events.EventEmitter {
+    private _amount: number;
+
+    constructor(amount: number) {
+        super();
+        this._amount = amount;
+    }
+
+    get amount() {
+        return Number(this._amount.toFixed(2));
+    }
+
+    set amount(amount: number) {
+        this._amount = amount;
+        this.emit("change", this._amount);
+    }
+}
