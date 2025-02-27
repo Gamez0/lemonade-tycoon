10f5be118f7b24615a565fea29d4ad4db3e752d9
dobin <dobinshin@gmail.com>
2025-02-27 22:50:19 +0900

    c: add get and methods

--- src/models/price.ts
+++ src/models/price.ts
@@ -7,11 +7,23 @@ export default class Price extends Phaser.Events.EventEmitter {
     }
 
     get amount() {
-        return this._amount;
+        return Number(this._amount.toFixed(2));
+    }
+
+    get amountString() {
+        return this._amount.toFixed(2);
     }
 
     set amount(amount: number) {
         this._amount = amount;
         this.emit("change", this._amount);
+    }
+
+    increase() {
+        this.amount += 0.1;
+    }
+
+    decrease() {
+        this.amount -= 0.1;
     }
 }
