12b9c7d1f6edb1ca1f48d3d7d6153d030c3c017c
dobin <dobinshin@gmail.com>
2025-03-24 23:12:49 +0900

    u: backgroundColor to #18ae31

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ const config: Types.Core.GameConfig = {
     width: 1024,
     height: 768,
     parent: "game-container",
-    backgroundColor: "#028af8",
+    backgroundColor: "#18ae31",
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
